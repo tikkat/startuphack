95ba711f5dec65188dfa8e3a9b19ba98e5478ed1
John Hult <johnhult89@gmail.com>
2015-02-18 21:25:23 +0100

    removed some unnec divs, removed outlines on buttonpress, fixed js to only react when actually pushing the img

--- res/scripts/main.js
+++ res/scripts/main.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
-		console.log($('arrow-down'));
-	$('.arrow-down').click(function() {
+	$('.arrow-down img').click(function() {
 	    $('html, body').animate({
 	        scrollTop: $('.intro').offset().top
 	    }, 1000);
