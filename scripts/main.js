9c8e1964d6803ae3ba1e3be87ec4e24b8a3973b9
John Hult <johnhult89@gmail.com>
2015-02-22 22:09:32 +0100

    Pretty done with last-year section

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,49 @@
+function isScrolledIntoView(elem) {
+    var $elem = $(elem);
+    var $window = $(window);
+
+    var docViewTop = $window.scrollTop();
+    var docViewBottom = docViewTop + $window.height();
+
+    var elemTop = $elem.offset().top;
+    var elemBottom = elemTop + $elem.height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+} 
+
+var stats;
 $(document).ready(function() {
+	stats = $('.statistics > h3');
 	$('.arrow-down img').click(function() {
 	    $('html, body').animate({
 	        scrollTop: $('.intro').offset().top
 	    }, 1000);
 	});
 });
+
+
+var statsDone = false;
+console.log(stats);
+$(document).on('scroll', function() {
+	if (isScrolledIntoView('.statistics') && !statsDone) {
+		(function countStats(i) {
+			statsDone = true;         
+			setTimeout(function () {
+		   		if (i<27) {
+					stats[0].innerHTML=i+1;
+				}
+				if (i<127) {
+					stats[1].innerHTML=i+1;
+				}
+				if (i<1) {
+					stats[2].innerHTML=i+1;
+				}
+				if (i<300) {
+					stats[3].innerHTML=i+2;
+				}
+				i = i+2;
+				if (i<300) countStats(i);
+		   	}, 10)
+		})(0);  
+	}
+})
