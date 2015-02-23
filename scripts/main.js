f0416e8d9d3613a20748fc582137fbe9c5773b1d
Tobias Tikka <tobias.tikka@gmail.com>
2015-02-23 19:31:22 +0100

    fixed javascript

--- scripts/main.js
+++ scripts/main.js
@@ -9,7 +9,7 @@ function isScrolledIntoView(elem) {
     var elemBottom = elemTop + $elem.height();
 
     return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-} 
+}
 
 var stats;
 $(document).ready(function() {
@@ -19,16 +19,22 @@ $(document).ready(function() {
 	        scrollTop: $('.intro').offset().top
 	    }, 1000);
 	});
-<<<<<<< Updated upstream
+
+  $('.the-day .ghost-btn').click(function(){
+		if($(this).hasClass('active'))
+			return;
+		else
+			$('.the-day .ghost-btn').each(function () {
+				$(this).toggleClass('active');
+		});
+	});
 });
 
-
 var statsDone = false;
-console.log(stats);
 $(document).on('scroll', function() {
 	if (isScrolledIntoView('.statistics') && !statsDone) {
 		(function countStats(i) {
-			statsDone = true;         
+			statsDone = true;
 			setTimeout(function () {
 		   		if (i<27) {
 					stats[0].innerHTML=i+1;
@@ -44,19 +50,7 @@ $(document).on('scroll', function() {
 				}
 				i = i+2;
 				if (i<300) countStats(i);
-		   	}, 10)
-		})(0);  
+      }, 10);
+		})(0);
 	}
-})
-=======
-
-	$('.the-day .ghost-btn').click(function(){
-		if($(this).hasClass('active'))
-			return;
-		else
-			$('.the-day .ghost-btn').each(function () {
-				$(this).toggleClass('active');
-		});
-	});
 });
->>>>>>> Stashed changes
